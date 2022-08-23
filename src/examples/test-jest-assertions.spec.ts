9e50c14e73bef73c66df4bbb35ce48838435b496
Pedro Augusto Ribeiro Silva <pedroars@outlook.com>
2022-08-23 13:38:36 -0300

    up

--- src/examples/test-jest-assertions.spec.ts
+++ src/examples/test-jest-assertions.spec.ts
@@ -3,7 +3,7 @@ describe('Primitive values', () => {
     const number = 10;
 
     expect(number).toBeLessThan(11);
-    expect(number).toBeLessThanOrEqual(10);
+    expect(number).toBeLessThanOrEqual(10)
 
     expect(number).toBeCloseTo(10.001);
     expect(number).toBeCloseTo(9.996);
@@ -11,7 +11,7 @@ describe('Primitive values', () => {
     expect(number).not.toBeNull();
 
     expect(number).toHaveProperty('toString');
-  });
+  })
 
   it('should split tests', () => {
     const number = 10;
@@ -24,19 +24,64 @@ describe('Primitive values', () => {
 
     expect(number).toBeGreaterThan(9);
     expect(number).toBeGreaterThanOrEqual(10);
-  });
-});
+  })
+})
 
 describe('Objects', () => {
-  it('should test jest assertions with objects', () => {
-    const person = { name: 'Luiz', age: 30 };
-    const anotherPerson = { ...person };
+  it('should test jest, assertions, with objects', () => {
+    const person = {name: 'Pedro', age: 30}
+    const anotherpPerson = {...person}
 
-    expect(person).toEqual(anotherPerson);
+    expect(person).toEqual(anotherpPerson);
     expect(person).toHaveProperty('age');
     expect(person).not.toHaveProperty('lastName');
     expect(person).toHaveProperty('age', 30);
 
-    expect(person.name).toBe('Luiz');
-  });
-});
+    expect(person.name).toBe('Pedro')
+  })
+})
+
+
+
+// describe('Primitive values', () => {
+//   it('should test jest assertions', () => {
+//     const number = 10;
+
+//     expect(number).toBeLessThan(11);
+//     expect(number).toBeLessThanOrEqual(10);
+
+//     expect(number).toBeCloseTo(10.001);
+//     expect(number).toBeCloseTo(9.996);
+
+//     expect(number).not.toBeNull();
+
+//     expect(number).toHaveProperty('toString');
+//   });
+
+//   it('should split tests', () => {
+//     const number = 10;
+
+//     expect(number).toBe(10);
+//     expect(number).toEqual(10);
+
+//     expect(number).not.toBeFalsy();
+//     expect(number).toBeTruthy();
+
+//     expect(number).toBeGreaterThan(9);
+//     expect(number).toBeGreaterThanOrEqual(10);
+//   });
+// });
+
+// describe('Objects', () => {
+//   it('should test jest assertions with objects', () => {
+//     const person = { name: 'Luiz', age: 30 };
+//     const anotherPerson = { ...person };
+
+//     expect(person).toEqual(anotherPerson);
+//     expect(person).toHaveProperty('age');
+//     expect(person).not.toHaveProperty('lastName');
+//     expect(person).toHaveProperty('age', 30);
+
+//     expect(person.name).toBe('Luiz');
+//   });
+// });
